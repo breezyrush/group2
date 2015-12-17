82999f9b72c91d3c8f164275c2f005fcebee8ffb
Marjun Makinano <marjunmakinano@gmail.com>
2015-12-17 19:21:14 +0800

    models.py

--- Problem/App/App/musixscore/models.py
+++ Problem/App/App/musixscore/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+	name = models.CharField()
+	def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+	
+class Performer(models.Model):
+	name = models.CharField()
+	genre_id = models.ManyToManyField(Genre, null=True, blank=True)
+	def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+		
+class Album(models.Model):
+	name = models.CharField()
+	def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+	
+class Track(models.Model):
+	name = models.CharField()
+	def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
