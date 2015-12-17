f9cd856ce4633697e9cf4701dec261e659f4a8b7
Mar Rynner Yee <marrynner.yee@g.msuiit.edu.ph>
2015-12-17 19:37:50 +0800

    Fixed some bugs in model.py

--- Problem/App/App/musixscore/models.py
+++ Problem/App/App/musixscore/models.py
@@ -2,36 +2,36 @@ from django.db import models
 
 
 class Genre(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length=50)
 	def __str__(self):
-        return self.name
+		return self.name
 
-    class Meta:
-        ordering = ['name']
+	class Meta:
+		ordering = ['name']
 	
 class Performer(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length=50)
 	genre_id = models.ManyToManyField(Genre, null=True, blank=True)
 	def __str__(self):
-        return self.name
+		return self.name
 
-    class Meta:
-        ordering = ['name']
+	class Meta:
+		ordering = ['name']
 		
 class Album(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length=50)
 	performer_id = models.ForeignKey(Performer, null=True, blank=True)
 	def __str__(self):
-        return self.name
+		return self.name
 
-    class Meta:
-        ordering = ['name']
+	class Meta:
+		ordering = ['name']
 	
 class Track(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length=50)
 	album_id = models.ForeignKey(Album, null=True, blank=True)
 	def __str__(self):
-        return self.name
+		return self.name
 
-    class Meta:
-        ordering = ['name']
+	class Meta:
+		ordering = ['name']
